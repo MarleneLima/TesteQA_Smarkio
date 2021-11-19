ae7681989a635be5315d82fdfda1f4e68fda7090
Marlene de Lima <88240817+MarleneLima@users.noreply.github.com>
2021-11-19 11:59:18 -0300

    Delete env.rb

--- Desafio/env.rb
+++
@@ -1,9 +0,0 @@
-require "capybara"
-require "capybara/cucumber"
-require "selenium-webdriver"
-
-Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
-  #config.app_host = 'https://www.youtube.com'
-  config.default_max_wait_time = 5
-end
